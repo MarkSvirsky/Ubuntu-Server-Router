317633d0b1f2284648dae3653aafa99919255041
Mark Svirsky <msvirski@ymail.com>
2026-03-18 02:48:22 +0200

    updated git-sync.sh logic

--- scripts/git-sync.sh
+++ scripts/git-sync.sh
@@ -30,7 +30,7 @@ if [ "$LOCAL" != "$REMOTE" ]; then
     fi
 
     # 3. Check if netplan changed
-    # Using a wildcard handle for the specific yaml filename
+    # Fix: Keep the '*' outside of quotes for expansion!
     if ! cmp -s "$REPO_PATH/configs/netplan/"*.yaml /etc/netplan/90-*.yaml; then
         echo "Updating Netplan..."
         sudo cp "$REPO_PATH/configs/netplan/"*.yaml /etc/netplan/
@@ -40,8 +40,9 @@ if [ "$LOCAL" != "$REMOTE" ]; then
     # 4. Check if routing/iptables config changed
     if ! cmp -s "$REPO_PATH/configs/routing/iptables.sh" "$IPTABLES_MARKER"; then
         echo "Updating Firewall/Routing..."
-        sudo bash "$REPO_PATH/configs/routing/iptables.sh"
-        # Update marker so we don't re-run every minute
+        # We use the 'Direct Binary' approach for cleaner sudoers
+        sudo iptables-restore < "$REPO_PATH/configs/routing/iptables.sh"
+        sudo sysctl -w net.ipv4.ip_forward=1
         sudo cp "$REPO_PATH/configs/routing/iptables.sh" "$IPTABLES_MARKER"
     fi
 
