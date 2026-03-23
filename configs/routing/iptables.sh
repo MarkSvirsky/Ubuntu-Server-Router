7c2bb75a6ec653825ba83550437f7408a516bcdd
MarkSvirsky <msvirski@ymail.com>
2026-03-23 22:07:06 +0200

    fix: restored ssh port 22 to iptables filter

--- configs/routing/iptables.sh
+++ configs/routing/iptables.sh
@@ -9,6 +9,7 @@ COMMIT
 :FORWARD ACCEPT [0:0]
 :OUTPUT ACCEPT [0:0]
 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -p tcp --dport 22 -j ACCEPT
 -A INPUT -p tcp --dport 8080 -j ACCEPT
 COMMIT
 
