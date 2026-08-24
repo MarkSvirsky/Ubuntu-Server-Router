6f04fa05c95e21bb7cb1c43e412f7daf1f1f0d00
Mark Svirsky <msvirski@ymail.com>
2026-08-24 23:03:53 +0300

    updated the script

--- scripts/backup-ssh-connection.sh
+++ scripts/backup-ssh-connection.sh
@@ -1,7 +1,3 @@
-# 1. Assign an IP to the USB adapter
-sudo ip addr add 192.168.1.200/24 dev enx503eaae32369
-
-# 2. Explicitly force 192.168.1.1 traffic through the USB adapter (bypassing D-Link)
-sudo ip route add 192.168.1.1/32 dev enx503eaae32369
+sudo ip addr add 10.99.1.2/24 dev enx503eaae32369
 
 # ssh to the server in case of netplan error via secondary network card
